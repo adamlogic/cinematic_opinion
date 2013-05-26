8a54ad605b8cf80dc8a8251811085df9926a9e72
Adam McCrea <adam@adamlogic.com>
2013-05-26 08:13:11 -0400

    Site updated to 4897ea5

--- javascripts/background.js
+++ javascripts/background.js
@@ -1,7 +1,1 @@
-(function() {
-
-  Chart.addChartBackground = function(svg) {
-    return svg.append('image').attr('width', Chart.width).attr('height', Chart.height).attr('x', Chart.margin.left).attr('y', 0).attr('xlink:href', '/images/chart_background.png');
-  };
-
-}).call(this);
+(function(){Chart.addChartBackground=function(e){return e.append("image").attr("width",Chart.width).attr("height",Chart.height).attr("x",Chart.margin.left).attr("y",0).attr("xlink:href","images/chart_background.png")}}).call(this);
